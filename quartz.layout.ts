6c420eea23c15727b4fc38a879adb593dc99a828
iomatix <mateuszwypchlak@gmail.com>
2024-11-01 18:11:28 +0100

    Quartz sync: Nov 1, 2024, 6:11 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -17,13 +17,13 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.PageTitle(),
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
+    Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.DesktopOnly(Component.Explorer()),
