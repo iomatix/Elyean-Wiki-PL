1687d4e42109c37b238574bbe9837ad3c5c426b7
iomatix <mateuszwypchlak@gmail.com>
2024-11-02 14:56:04 +0100

    vault backup: 2024-11-02 14:56:04
    Affected files:
    content/.makemd/fileCache.mdc
    content/.makemd/superstate.mdc
    content/.space/context.mdb
    content/Jira/sprint-Dev-2/Dev-2-Board.md
    content/Jira/sprint-Dev-2/Quest - PEZE-119.md
    content/Jira/sprint-Dev-2/Quest - PEZE-120.md
    content/Jira/sprint-Dev-2/Quest - PEZE-121.md
    content/Jira/sprint-Dev-2/Quest - PEZE-122.md
    content/Jira/sprint-Dev-2/Quest - PEZE-123.md
    content/Jira/sprint-Dev-2/Quest - PEZE-124.md
    content/Jira/sprint-Dev-2/Quest - PEZE-125.md
    content/Jira/sprint-Dev-2/Quest - PEZE-126.md
    content/Jira/sprint-Dev-2/Quest - PEZE-127.md
    content/Jira/sprint-Dev-2/Quest - PEZE-128.md
    content/Jira/sprint-Dev-2/Quest - PEZE-130.md
    content/Jira/sprint-Dev-2/Quest - PEZE-131.md
    content/Jira/sprint-Dev-2/Quest - PEZE-132.md
    content/Jira/sprint-Dev-2/Undertaking - PEZE-129.md
    quartz/components/NewestFiles.tsx

--- quartz/components/NewestFiles.tsx
+++ quartz/components/NewestFiles.tsx
@@ -1,12 +1,14 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/newestFiles.scss"
+import { formatDate, getDate } from "./Date"
 import { resolveRelative } from "../util/path"
 import { i18n } from "../i18n"
 import { classNames } from "../util/lang"
 
 // Helper function to sort files by date
 const sortByDateDescending = (a: any, b: any) => {
-  const dateA = new Date(a.frontmatter?.date || 0).getTime()
+  const dateA = getDate(cfg, fileData)!, cfg.locale).getTime()
+  new Date(a.frontmatter?.date || 0).getTime()
   const dateB = new Date(b.frontmatter?.date || 0).getTime()
   return dateB - dateA // Newest files first
 }
@@ -53,7 +55,9 @@ const NewestFiles: QuartzComponent = ({
           newestFiles.map((file) => (
             <li>
               <a href={resolveRelative(fileData.slug!, file.slug!)} class="internal">
-              {file.frontmatter?.title} - {new Date(file.dates?.[validNewestType as keyof typeof file.dates] || new Date()).toLocaleString()}
+              {file.frontmatter?.title} - {formatDate(getDate(cfg, fileData)!, cfg.locale)}
+              
+              {new Date(file.dates?.[validNewestType as keyof typeof file.dates] || new Date()).toLocaleString()}
               </a>
             </li>
           ))
