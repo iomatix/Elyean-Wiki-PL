ceccac06beed90db55a063811b83437cee72a160
iomatix <mateuszwypchlak@gmail.com>
2024-11-02 15:48:38 +0100

    Quartz sync: Nov 2, 2024, 3:48 PM

--- quartz/components/NewestFiles.tsx
+++ quartz/components/NewestFiles.tsx
@@ -1,26 +1,29 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/newestFiles.scss"
+import { ValidDateType } from "./Date"
 import { formatDate, getDate } from "./Date"
 import { resolveRelative } from "../util/path"
 import { i18n } from "../i18n"
 import { classNames } from "../util/lang"
 
+
+
 // Helper function to sort files by date
-const sortByDateDescending = (a: any, b: any) => {
-  const dateA = new Date(a.date || 0).getTime()
-  const dateB = new Date(b.date || 0).getTime()
-  return dateB - dateA // Newest files first
-}
+const sortByDateDescending = (type: ValidDateType) => (a: any, b: any) => {
+  const dateA = a.dates[type]
+  const dateB = b.dates[type]
+  return dateB.getTime() - dateA.getTime();
+};
+
 
 // Interface Declaration
 interface Options {
-  newestType: "published" | "modified" | "created"
+  newestType?: ValidDateType
   maxFiles: number
 }
 
 // Default Interface Values
 const defaultOptions: Options = {
-  newestType: "published",
   maxFiles: 5,
 }
 
@@ -35,26 +38,26 @@ const NewestFiles: QuartzComponent = ({
   // Use defaults if not in userOpts
   const options: Options = { ...defaultOptions, ...inputOptions }
   // Validation of newestType, default = cfg default DataType
-  const validNewestType = ["published", "modified", "created"].includes(options.newestType) ? options.newestType : cfg.defaultDateType
+  const validNewestType: ValidDateType = options.newestType ? options.newestType : cfg.defaultDateType
 
   // Validation of maxFiles, default = 5
   const validMaxFiles = options.maxFiles > 0 ? options.maxFiles : 5
 
   // Sort all files by date and pick the top X newest files
   const newestFiles = allFiles
-    .filter((file) => file.dates?.[validNewestType as keyof typeof file.dates])
-    .sort(sortByDateDescending)
+    .filter((file) => file.dates?.[validNewestType])
+    .sort(sortByDateDescending(validNewestType))
     .slice(0, validMaxFiles)
   
   return (
     <div class={classNames(displayClass, "newestFiles")}>
-      <h3>{i18n(cfg.locale).components.newestFiles.title[validNewestType as "published" | "modified" | "created"]}</h3>
+      <h3>{i18n(cfg.locale).components.newestFiles.title[validNewestType]}</h3>
       <ul class="overflow">
         {newestFiles.length > 0 ? (
           newestFiles.map((file) => (
             <li>
               <a href={resolveRelative(fileData.slug!, file.slug!)} class="internal">
-              {file.frontmatter?.title} - {formatDate((file.dates?.[validNewestType as keyof typeof file.dates] || new Date(0)), cfg.locale)}
+              {file.frontmatter?.title} - {formatDate((file.dates![validNewestType]), cfg.locale)}
               </a>
             </li>
           ))
